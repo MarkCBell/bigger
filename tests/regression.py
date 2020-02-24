f278775b9a1930b4859281789b80f041a8524b31
Mark Bell <mark00bell@googlemail.com>
2020-02-24 23:24:58 +0000

    Tests use new notation.

--- tests/regression.py
+++ tests/regression.py
@@ -15,19 +15,19 @@ class TestRegression(TestCase):
         return s1 == s2 or s1 == s2[2:] + s2[:2]
     
     def test_twist(self):
-        self.assertEqual(self.S('a0')(self.a), self.a)
-        self.assertEqual(self.S('A0.a0')(self.m), self.m)
-        self.assertEqual(self.S('A0.a0')(self.m2), self.m2)
+        self.assertEqual(self.S('a_0')(self.a), self.a)
+        self.assertEqual(self.S('A_0.a_0')(self.m), self.m)
+        self.assertEqual(self.S('A_0.a_0')(self.m2), self.m2)
     
     def test_flip(self):
         h = self.S.triangulation.encode([{0}, {0}])
         self.assertEqualSquares(h.target.link(0), h.source.link(0))
     
     def test_explicit(self):
-        self.assertEqual((self.S('a0')**1)(self.b), {2: 1})
-        self.assertEqual((self.S('a0')**2)(self.b), {1: 1, 2: 2})
-        self.assertEqual((self.S('a0')**10)(self.b), {1: 9, 2: 10})
-        self.assertEqual((self.S('s.a0.a0')**10)(self.b), {32: 2, 31: 1})
+        self.assertEqual((self.S('a_0')**1)(self.b), {2: 1})
+        self.assertEqual((self.S('a_0')**2)(self.b), {1: 1, 2: 2})
+        self.assertEqual((self.S('a_0')**10)(self.b), {1: 9, 2: 10})
+        self.assertEqual((self.S('s.a_0.a_0')**10)(self.b), {32: 2, 31: 1})
 
     def test_infinite_twist_commutes(self):
         s = self.S('s')
