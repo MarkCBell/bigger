96de2b1519b462e02b82945f1df0b0309bbd3f7f
Mark Bell <mark00bell@googlemail.com>
2020-03-07 17:08:18 +0000

    Added tests to increase coverage.

--- tests/regression.py
+++ tests/regression.py
@@ -1,13 +1,44 @@
 
 from unittest import TestCase
 import bigger
+from itertools import islice
 
-class TestRegression(TestCase):
+class TestFlute(TestCase):
+    S = bigger.load.flute()
+    T = S.triangulation
+    L = T.as_lamination()
+    edges = list(islice(T, 10))
+    a = T({edges[0]: -1})
+    b = T({edges[2]: -1})
+    m = T(dict((e, -i) for i, e in enumerate(edges)))
+    
+    def test_str(self):
+        self.assertEqual(str(self.L), 'Infinitely supported lamination 0: -1, -1: -1, 1: -1, -2: -1, 2: -1, -3: -1, 3: -1, -4: -1, 4: -1, -5: -1 ...')
+        self.assertEqual(str(self.a), 'Lamination 0: -1')
+        self.assertEqual(repr(self.L), str(self.L))
+    
+    def test_twist(self):
+        self.assertEqual(self.S('b_1')(self.L).show(range(10)), '0: -1, 1: 6, 2: 7, 3: 14, 4: 19, 5: -1, 6: 14, 7: 6, 8: 7, 9: -1')
+        self.assertEqual(self.S('a')(self.L).show(range(10)), '0: -1, 1: -1, 2: 1, 3: -1, 4: -1, 5: 1, 6: -1, 7: -1, 8: 1, 9: -1')
+        self.assertEqual(self.S('a_4.a.B_2')(self.L).show(range(10)), '0: -1, 1: -1, 2: 1, 3: -1, 4: 7, 5: 8, 6: 16, 7: -1, 8: 21, 9: 16')
+    
+    def test_flip(self):
+        self.assertEqual(self.S('a_0')(self.T({1: 4, 2: 2, 3: 4})), {1: 2, 2: 1, 3: 4})
+        self.assertEqual(self.S('a_0')(self.T({0: 4, 1: 4, 2: 2})), {0: 4, 1: 2, 2: 1})
+        self.assertEqual(self.T.encode([1])(self.a), {0: -1})
+    
+    def test_slice(self):
+        h = self.S('a.a.b_1.a_3')
+        self.assertEqual((h[:5] * h[5:])(self.m), h(self.m))
+
+class TestBiflute(TestCase):
     S = bigger.load.biflute()
-    a = S.triangulation({0: -1})
-    b = S.triangulation({1: -1})
-    m = S.triangulation({1: -1})
-    m2 = S.triangulation(dict((i, -i) for i in range(10)))
+    T = S.triangulation
+    L = T.as_lamination()
+    edges = list(islice(T, 10))
+    a = T({edges[0]: -1})
+    b = T({edges[2]: -1})
+    m = T(dict((e, -i) for i, e in enumerate(edges)))
     
     def assertEqualSquares(self, s1, s2):
         ''' Return whether two squares are equal since they are only defined up to 180 degree rotation. '''
@@ -17,13 +48,18 @@ class TestRegression(TestCase):
     def test_twist(self):
         self.assertEqual(self.S('a_0')(self.a), self.a)
         self.assertEqual(self.S('A_0.a_0')(self.m), self.m)
-        self.assertEqual(self.S('A_0.a_0')(self.m2), self.m2)
+        self.assertEqual(self.S('b_1')(self.b), {2: 1, 3: 1, 4: 2, 6: 2, 7: 1, 8: 1})
     
     def test_flip(self):
-        h = self.S.triangulation.encode([{0}, {0}])
+        h = self.T.encode([{0}, {0}])
         self.assertEqualSquares(h.target.link(0), h.source.link(0))
     
-    def test_explicit(self):
+    def test_expr(self):
+        h = self.S('a{n > 7 or n % 6 == 3}')
+        self.assertEqual(h(self.m), {1: -2, 2: -4, 3: -6, 4: -8, -2: -3, -5: -9, -4: -7, -3: -5, -1: -1})
+    
+    def test_powers(self):
+        self.assertEqual((self.S('a_0.a.b_1')**0)(self.b), self.b)
         self.assertEqual((self.S('a_0')**1)(self.b), {2: 1})
         self.assertEqual((self.S('a_0')**2)(self.b), {1: 1, 2: 2})
         self.assertEqual((self.S('a_0')**10)(self.b), {1: 9, 2: 10})
@@ -34,4 +70,40 @@ class TestRegression(TestCase):
         t = self.S('a')
         self.assertEqual((t * s**1)(self.b), (s**1 * t)(self.b))
         self.assertEqual((t * s**10)(self.b), (s**10 * t)(self.b))
+
+class TestLadder(TestCase):
+    S = bigger.load.ladder()
+    T = S.triangulation
+    L = T.as_lamination()
+    edges = list(islice(T, 10))
+    a = T({edges[0]: -1})
+    b = T({edges[2]: -1})
+    m = T(dict((e, -i) for i, e in enumerate(edges)))
+    
+    def test_str(self):
+        self.assertEqual(str(self.L), 'Infinitely supported lamination (0, 0): -1, (0, 1): -1, (0, 2): -1, (0, 3): -1, (0, 4): -1, (0, 5): -1, (0, 6): -1, (0, 7): -1, (0, 8): -1, (-1, 0): -1 ...')
+        self.assertEqual(str(self.a), 'Lamination (0, 0): -1')
+        self.assertEqual(repr(self.L), str(self.L))
+    
+    def test_twist(self):
+        self.assertEqual(self.S('a_1')(self.m), {(0, 1): -1, (0, 7): -7, (0, 4): -4, (0, 3): -3, (0, 6): -6, (-1, 0): -9, (0, 2): -2, (0, 5): -5, (0, 8): -8})
+        self.assertEqual(self.S('a.b.s.s.a_1.B_1')(self.m), self.S('a_3.s.b.a.s.B_1')(self.m))
+
+class TestTree3(TestCase):
+    S = bigger.load.tree3()
+    T = S.triangulation
+    L = T.as_lamination()
+    edges = list(islice(T, 10))
+    a = T({edges[0]: -1})
+    b = T({edges[2]: -1})
+    m = T(dict((e, -i) for i, e in enumerate(edges)))
+    
+    def test_str(self):
+        self.assertEqual(str(self.L), 'Infinitely supported lamination (0, 0): -1, (0, 1): -1, (0, 2): -1, (0, 3): -1, (-1, 0): -1, (-1, 1): -1, (-1, 2): -1, (-1, 3): -1, (1, 0): -1, (1, 1): -1 ...')
+        self.assertEqual(str(self.a), 'Lamination (0, 0): -1')
+        self.assertEqual(repr(self.L), str(self.L))
+    
+    def test_twist(self):
+        self.assertEqual(self.S('a_1')(self.m), {(0, 1): -1, (-1, 1): -5, (1, 1): 8, (0, 3): -3, (-1, 0): -4, (0, 2): -2, (-1, 3): -7, (1, 0): -9, (-1, 2): -6})
+        self.assertEqual(self.S('a.a.A_1')(self.m), self.S('A_1.a.a')(self.m))
 
