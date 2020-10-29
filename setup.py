acf48c5c53273a9f8c6855b16275684f28009377
Mark Bell <mark00bell@googlemail.com>
2020-10-29 11:09:13 +0000

    Updated version number.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def readme():
 
 setup(
     name='bigger',
-    version='0.2.2',
+    version='0.2.3',
     description='For studying big mapping classes',
     long_description=readme(),
     author='Mark Bell',
