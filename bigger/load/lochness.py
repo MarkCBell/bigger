0c131e4bfe15249a5ea84953a9d1a3cc3e71a7af
Mark Bell <mark00bell@googlemail.com>
2021-06-21 00:54:06 +0100

    Moved loch_ness_monster to just use integers.

--- bigger/load/lochness.py
+++ bigger/load/lochness.py
@@ -2,14 +2,12 @@
 
 from __future__ import annotations
 
-from typing import Tuple, Iterable
-
 import bigger
 from bigger.types import FlatTriangle
 from bigger.triangulation import Triangle
 from .utils import integers, extract_curve_and_test
 
-Edge = Tuple[int, int]
+Edge = int
 
 
 def loch_ness_monster() -> bigger.MCG[Edge]:
@@ -22,39 +20,34 @@ def loch_ness_monster() -> bigger.MCG[Edge]:
      - c which twists about the curves linking the nth and n+1st handles
      - s which shifts the surface down"""
 
-    #  #---n,2---#
+    #  #---n+2---#
     #  |        /|
     #  |      /  |
-    # n,0  n,1 n+1,0
+    #  n   n+1  n+6
     #  |  /      |
     #  |/        |
-    #  #---n,3---#
+    #  #---n+3---#
     #  |        /|
     #  |      /  |
-    # n,4  n,5  n,4
+    # n+4  n+5  n+4
     #  |  /      |
     #  |/        |
-    #  #---n,2---#
-
-    def edges() -> Iterable[Edge]:
-        for x in integers():
-            for y in range(6):
-                yield x, y
+    #  #---n+2---#
 
     def link(edge: Edge) -> tuple[Edge, bool, Edge, bool, Edge, bool, Edge, bool]:
-        n, k = edge
+        n, k = divmod(edge, 6)
         return {
-            0: ((n - 1, 1), False, (n - 1, 3), True, (n, 1), True, (n, 2), False),
-            1: ((n, 2), False, (n, 0), False, (n, 3), True, (n + 1, 0), True),
-            2: ((n, 4), True, (n, 5), False, (n, 0), False, (n, 1), True),
-            3: ((n + 1, 0), True, (n, 1), False, (n, 4), False, (n, 5), True),
-            4: ((n, 5), False, (n, 2), True, (n, 5), True, (n, 3), False),
-            5: ((n, 3), False, (n, 4), False, (n, 2), True, (n, 4), True),
+            0: (6 * n - 6 + 1, False, 6 * n - 6 + 3, True, 6 * n + 1, True, 6 * n + 2, False),
+            1: (6 * n + 2, False, 6 * n, False, 6 * n + 3, True, 6 * n + 6, True),
+            2: (6 * n + 4, True, 6 * n + 5, False, 6 * n, False, 6 * n + 1, True),
+            3: (6 * n + 6, True, 6 * n + 1, False, 6 * n + 4, False, 6 * n + 5, True),
+            4: (6 * n + 5, False, 6 * n + 2, True, 6 * n + 5, True, 6 * n + 3, False),
+            5: (6 * n + 3, False, 6 * n + 4, False, 6 * n + 2, True, 6 * n + 4, True),
         }[k]
 
-    T = bigger.Triangulation[Edge].from_pos(edges, link)
+    T = bigger.Triangulation[Edge].from_pos(integers, link)
 
-    shift = T.isometry(T, lambda edge: (edge[0] + 1, edge[1]), lambda edge: (edge[0] - 1, edge[1]))
+    shift = T.isometry(T, lambda edge: edge + 6, lambda edge: edge - 6)
 
     def generator(name: str) -> bigger.Encoding[Edge]:
         if name in ("s", "shift"):
@@ -63,13 +56,13 @@ def loch_ness_monster() -> bigger.MCG[Edge]:
         curve, test = extract_curve_and_test("abc", name)
 
         if curve == "a":
-            return T(lambda edge: 1 if edge[1] in {1, 2, 3, 5} and test(edge[0]) else 0).twist()
+            return T(lambda edge: 1 if edge % 6 in {1, 2, 3, 5} and test(edge // 6) else 0).twist()
         if curve == "b":
-            return T(lambda edge: 1 if edge[1] in {4, 5} and test(edge[0]) else 0).twist()
+            return T(lambda edge: 1 if edge % 6 in {4, 5} and test(edge // 6) else 0).twist()
         if curve == "c":
 
             def c(edge: Edge) -> int:
-                n, k = edge
+                n, k = divmod(edge, 6)
                 if test(n) and test(n + 1):
                     return {0: 2, 1: 4, 2: 2, 3: 2, 4: 2, 5: 2}[k]
                 elif test(n):
@@ -84,7 +77,7 @@ def loch_ness_monster() -> bigger.MCG[Edge]:
         raise ValueError("Unknown mapping class {}".format(name))
 
     def layout(triangle: Triangle[Edge]) -> FlatTriangle:
-        n, k = triangle[0].edge
+        n, k = divmod(triangle[0].edge, 6)
         return {
             0: ((n, 2.0), (n, 1.0), (n + 1.0, 2.0)),
             1: ((n + 1.0, 2.0), (n, 1.0), (n + 1.0, 1.0)),
